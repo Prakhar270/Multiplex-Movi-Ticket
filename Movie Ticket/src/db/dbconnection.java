3bd0098631a0166140af3a3e027acbd238e3bd76
Prakhar <Prakhar@gmail.com>
2023-06-17 12:27:58 +0530

    The Database and Code

---
+++ Movie Ticket/src/db/dbconnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.sql.*;
+
+public class dbconnection {
+    public Statement stmt=null;
+    public Connection con=null;
+    public PreparedStatement pstmt=null;
+    public ResultSet rst=null;
+    public dbconnection()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/movie_ticket","root","pk123");
+        }
+        catch(ClassNotFoundException | SQLException e){
+            e.getMessage();
+         }
+    }
+}
